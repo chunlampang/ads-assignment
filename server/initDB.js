ccc191944b2526dc14b9f34d1e78c806f47b15d6
Pang Chun Lam <30290516+chunlampang@users.noreply.github.com>
2019-06-13 16:53:17 +0800

    createCollection

--- server/initDB.js
+++ server/initDB.js
@@ -7,9 +7,18 @@ module.exports = async function initDB() {
     const db = await MongoClient.connect(url);
     const dbo = db.db("adsdb");
 
-    if (!dbo.getCollection('Offer').exists())
-      await dbo.createCollection("Offer");
-    await dbo.getCollection('Offer').ensureIndex("Year");
+    if (!dbo.getCollection('departments').exists())
+      await dbo.createCollection("departments");
+
+    if (!dbo.getCollection('courses').exists())
+      await dbo.createCollection("courses");
+
+    if (!dbo.getCollection('students').exists())
+      await dbo.createCollection("students");
+
+    if (!dbo.getCollection('offer').exists())
+      await dbo.createCollection("offer");
+    await dbo.getCollection('offer').ensureIndex("year");
 
     db.close();
   } catch (err) {
