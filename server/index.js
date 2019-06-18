f6570ac8d541a45a81855039dc1a4d2dba440e65
Pang Chun Lam <30290516+chunlampang@users.noreply.github.com>
2019-06-18 18:18:27 +0800

    static route for vue build

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const history = require('connect-history-api-fallback');
 const moment = require('moment');
 const mongoPool = require.main.require('./utils/mongoPool');
 
-(async function init(){
+(async function init() {
   await mongoPool.connect();
 
   const app = express();
-  
+  //api
   app.use('/api', function (req, res, next) {
     console.log(moment().format('YYYY-MM-DD HH:mm:ss'), req.ip, req.method, req.query);
     next();
   }, require('./api'));
-  
-  app.use(express.static('public'));
-  
+
+  //static route
+  const staticFileMiddleware = express.static('public');
+  app.use(staticFileMiddleware);
+  app.use(history());
+  app.use(staticFileMiddleware);
+
+  //start server
   app.listen(8080, function () {
     console.log('Server start listening on http://localhost:8080');
   });
-  
+
 })();
