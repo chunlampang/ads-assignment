1d157d5565004d4ba6acfbb409544f60c0ed9e84
Pang Chun Lam <30290516+chunlampang@users.noreply.github.com>
2019-06-18 12:54:08 +0800

    add data

--- server/data/offers.data.js
+++ server/data/offers.data.js
@@ -3,14 +3,15 @@ module.exports = [
         department: 'CS', course: 'CS101', year: 2016,
         classSize: 40, availablePlaces: 39,
         enrolled: [
-            { student: '15101010', enrolDate: new Date('2016-05-15') }
+            { student: '15101010', enrolDate: new Date('2016-05-15') },
+            { student: '15101011', enrolDate: new Date('2016-05-16') },
         ]
     },
     {
         department: 'CS', course: 'CS102', year: 2016,
         classSize: 40, availablePlaces: 39,
         enrolled: [
-            { student: '15101010', enrolDate: new Date('2016-05-15') }
+            { student: '15101010', enrolDate: new Date('2016-05-15') },
         ]
     },
     {
--- server/data/students.data.js
+++ server/data/students.data.js
@@ -1,5 +1,4 @@
 module.exports = [
-    {
-        _id: '15101010', stuName: 'Chan Tai Man', dOB: new Date('2019-08-10')
-    }
+    {_id: '15101010', stuName: 'Chan Tai Man', dOB: new Date('2019-08-10')},
+    {_id: '15101011', stuName: 'Man Tai Man', dOB: new Date('2019-01-10')},
 ]
