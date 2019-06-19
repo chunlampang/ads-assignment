c06a6e6143b3967fd5639ba680d5467f5a20e03e
Pang Chun Lam <30290516+chunlampang@users.noreply.github.com>
2019-06-19 11:15:32 +0800

    log

--- server/api/index.js
+++ server/api/index.js
@@ -4,12 +4,14 @@ const moment = require('moment');
 const api = module.exports = express.Router({ mergeParams: true });
 
 api.use((req, res, next) => {
-  console.log(moment().format('YYYY-MM-DD HH:mm:ss'), req.ip, req.method, req.query);
+  console.log(moment().format('YYYY-MM-DD HH:mm:ss'),
+    req.ip, req.method, req.path, req.query);
+
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
 require("fs").readdirSync(__dirname).forEach((file) => {
-    if (file !== 'index.js')
-        require("./" + file)(api);
+  if (file !== 'index.js')
+    require("./" + file)(api);
 });
