5f68b19e2e8cd6611c41a31783f6143b61f4adfd
Pang Chun Lam <30290516+chunlampang@users.noreply.github.com>
2019-06-19 11:16:26 +0800

    rename home page

--- vue/src/router.js
+++ vue/src/router.js
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'index',
-      component: () => import('@/components/pages/HelloWorld')
+      component: () => import('@/components/pages/Index')
     },
     {
       path: ':path*',
