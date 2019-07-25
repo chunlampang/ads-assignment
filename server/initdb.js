3f7a5407783a70f8c4103d36c2d7457b5ad941be
Pang Chun Lam <30290516+chunlampang@users.noreply.github.com>
2019-07-25 12:56:10 +0800

    initdb.js

---
+++ server/initdb.js
@@ -0,0 +1,39 @@
+
+const mongoPool = require('./utils/mongoPool');
+
+(async function () {
+    try {
+        const db = await mongoPool.getDb();
+        console.log('Reseting database');
+        await db.dropDatabase();
+
+        async function insert(collectionName, data, created) {
+            let collection = await db.createCollection(collectionName);
+            console.log(`Created collection: ${collectionName}`);
+            if (created)
+                await created(collection);//for create indexs
+            //insert data
+            let res = await collection.insertMany(data, { w: 1 });
+            if (res.result.ok)
+                console.log(`Inserted ${collectionName} data`);
+            return res;
+        }
+
+        await Promise.all([
+            insert('departments', require('./data/departments.data')),
+            insert('courses', require('./data/courses.data')),
+            insert('students', require('./data/students.data'), collection => {
+                collection.createIndex({ stuName: 1 });
+            }),
+            insert('offers', require('./data/offers.data'), collection => {
+                collection.createIndex({ department: 1, course: 1, year: 1 }, { unique: true });
+                collection.createIndex({ enrolledCount: 1 });
+            }),
+        ]);
+        console.log('All Completed');
+    } catch (err) {
+        console.error(err.message);
+    } finally {
+        process.exit();
+    }
+})();
