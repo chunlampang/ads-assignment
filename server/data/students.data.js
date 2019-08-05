67b5b255ba6119b45cf0b2b062b29e0fa41397b2
Pang Chun Lam <chunlampang@gmail.com>
2019-08-05 22:44:05 +0800

    title case

--- server/data/students.data.js
+++ server/data/students.data.js
@@ -1,35 +1,10 @@
 const stuNames = [
-    'Chan Tai Man',
-    'CHAN Kwan Kin', 'CHAN Tsz Him', 'CHAN Yat Fung', 'CHIU Man Ki',
-    'CHENG Ka Lung', 'CHOI Pui Ki', 'CHIU Wan Kei', 'CHUNG Tsz Wai',
-    'CHO Ka Shing', 'HUNG Chi Wai', 'CHOI Yue Chung', 'FOK Ho Sum',
-    'HO Tsz Wai', 'LAI Ka Shing', 'IP Cheuk Hong', 'LAI Kwok On',
-    'LAM Chit Fai', 'LAM Chi Keung', 'LAM Ting Yin', 'LAU Pok Him',
-    'LI Wing Yee', 'LI Chun Yin', 'LIU Tat Wing', 'LIN Yunsong',
-    'MEHMOOD Asif', 'SU Qiping', 'NG Kwok Wah', 'TAM Ching Lam',
-    'SHEK Sai Tai', 'TIU Chun Yiu', 'TAM Ying Kit', 'TONG Wai Ho',
-    'WONG Pui Shan', 'WONG Chor Ying', 'YAM Hoi Ming Herman', 'WONG Hoi Ping',
-    'YIU Ho Man', 'CHAN Chun Ho', 'IP Chi Yat', 'CHAN Hoi Fu',
-    'Chan Tai Man',
-    'KEUNG Man Ho Martin', 'CHAN Kwan Yin', 'KWAN Sing Him', 'CHAN Wai Chun',
-    'LAM Ho Ching', 'CHAN Yeung Kwan', 'LAM Ka Suen', 'CHAO Tsz Chung',
-    'LAU Chun Chi', 'CHAU Ha Wai', 'LAU Ka Wai', 'CHEN Po Han',
-    'LAU King Sum', 'CHENG Ka Lok', 'LAW Cheuk Ying', 'CHEUNG Hoi Ching',
-    'LAW King Sang', 'CHEUNG Wai Pang', 'LAW Shing Him', 'CHIU Wing Hong',
-    'LAW Yiu On', 'CHONG Sau Hung', 'LEE Ka Wai', 'CHOW Pui Man',
-    'LEUNG Kai Chung', 'CHU Kam Kuen', 'LEUNG Siu On', 'DALUZ Kier Jose',
-    'LI Chi Shing', 'DENG Jia Yi', 'LI Man Hin', 'FOK Chi Chung',
-    'LIONG Ming Sun', 'FUNG Wing Lok', 'LIU Man Chung', 'HON Yu Fung',
-    'LO Ka Hei', 'HONG Kit Fung', 'LO Tsz Shing', 'WONG Ming Tim',
-    'LUK Ching Wa', 'MA Koi Yin', 'MAK Ka Him', 'MAN Ho Fung',
-    'MAN Ka Kit', 'NG Po Yin', 'NG Sze Yeung Chris', 'NG Wai Ki',
-    'NG Yiu Chuen', 'PANG Chun Lam', 'RAI Nischal', 'TAO Yeh',
-    'TSANG Chun Ho', 'TSUI Christopher Lap Yin', 'TSUI Yik Fai', 'WAN Ho Yin',
-    'WONG Kin Yu', 'WONG Kwok Tai', 'WONG Pan Pan', 'WONG Tsz Yui',
-    'WONG Yan Wai', 'WONG Yuen Wa', 'XU Siu Hin', 'YEUNG Chun Ho',
-    'YEUNG Hok Sum', 'YIONG Hei Tung Marcia',
-    'Chan Tai Man',
-]
+    "Chan Tai Man",
+    "Chan Kwan Kin", "Chan Tsz Him", "Chan Yat Fung", "Chiu Man Ki", "Cheng Ka Lung", "Choi Pui Ki", "Chiu Wan Kei", "Chung Tsz Wai", "Cho Ka Shing", "Hung Chi Wai", "Choi Yue Chung", "Fok Ho Sum", "Ho Tsz Wai", "Lai Ka Shing", "Ip Cheuk Hong", "Lai Kwok On", "Lam Chit Fai", "Lam Chi Keung", "Lam Ting Yin", "Lau Pok Him", "Li Wing Yee", "Li Chun Yin", "Liu Tat Wing", "Lin Yunsong", "Mehmood Asif", "Su Qiping", "Ng Kwok Wah", "Tam Ching Lam", "Shek Sai Tai", "Tiu Chun Yiu", "Tam Ying Kit", "Tong Wai Ho", "Wong Pui Shan", "Wong Chor Ying", "Yam Hoi Ming Herman", "Wong Hoi Ping", "Yiu Ho Man", "Chan Chun Ho", "Ip Chi Yat", "Chan Hoi Fu",
+    "Chan Tai Man",
+    "Keung Man Ho Martin", "Chan Kwan Yin", "Kwan Sing Him", "Chan Wai Chun", "Lam Ho Ching", "Chan Yeung Kwan", "Lam Ka Suen", "Chao Tsz Chung", "Lau Chun Chi", "Chau Ha Wai", "Lau Ka Wai", "Chen Po Han", "Lau King Sum", "Cheng Ka Lok", "Law Cheuk Ying", "Cheung Hoi Ching", "Law King Sang", "Cheung Wai Pang", "Law Shing Him", "Chiu Wing Hong", "Law Yiu On", "Chong Sau Hung", "Lee Ka Wai", "Chow Pui Man", "Leung Kai Chung", "Chu Kam Kuen", "Leung Siu On", "Daluz Kier Jose", "Li Chi Shing", "Deng Jia Yi", "Li Man Hin", "Fok Chi Chung", "Liong Ming Sun", "Fung Wing Lok", "Liu Man Chung", "Hon Yu Fung", "Lo Ka Hei", "Hong Kit Fung", "Lo Tsz Shing", "Wong Ming Tim", "Luk Ching Wa", "Ma Koi Yin", "Mak Ka Him", "Man Ho Fung", "Man Ka Kit", "Ng Po Yin", "Ng Sze Yeung Chris", "Ng Wai Ki", "Ng Yiu Chuen", "Pang Chun Lam", "Rai Nischal", "Tao Yeh", "Tsang Chun Ho", "Tsui Christopher Lap Yin", "Tsui Yik Fai", "Wan Ho Yin", "Wong Kin Yu", "Wong Kwok Tai", "Wong Pan Pan", "Wong Tsz Yui", "Wong Yan Wai", "Wong Yuen Wa", "Xu Siu Hin", "Yeung Chun Ho", "Yeung Hok Sum", "Yiong Hei Tung Marcia",
+    "Chan Tai Man"
+];
 
 const students = [];
 
