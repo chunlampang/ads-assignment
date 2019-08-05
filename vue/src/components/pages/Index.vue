8020385cac1994f243af6f5fa6fe6de4d7c83d72
Pang Chun Lam <chunlampang@gmail.com>
2019-08-05 10:56:29 +0800

    model tree for present

--- vue/src/components/pages/Index.vue
+++ vue/src/components/pages/Index.vue
@@ -12,5 +12,150 @@
         </v-card-text>
       </v-card>
     </v-flex>
+    <v-flex xs12>
+      <v-divider class="primary my-4"></v-divider>
+    </v-flex>
+    <v-flex xs12>
+      <v-container grid-list-md class="px-0">
+        <v-layout>
+          <v-flex xs6>
+            <v-card>
+              <v-card-title>
+                <div class="headline">Model 1</div>
+              </v-card-title>
+              <v-card-text>
+                <v-treeview :items="model1" dense open-on-click open-all></v-treeview>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex xs6>
+            <v-card>
+              <v-card-title>
+                <div class="headline">Model 2</div>
+              </v-card-title>
+              <v-card-text>
+                <v-treeview :items="model2" dense open-on-click open-all></v-treeview>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-flex>
   </v-layout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      model1: [
+        {
+          id: 1,
+          name: "departments",
+          children: [
+            { name: '_id: "ObjectId"' },
+            { name: 'deptId: "CS"' },
+            { name: 'deptName: "Computer Science"' },
+            { name: 'location: "Green Zone"' }
+          ]
+        },
+        {
+          id: 2,
+          name: "courses",
+          children: [
+            { name: '_id: "ObjectId"' },
+            { name: 'courseId: "CS101"' },
+            { name: 'title: "Introduction to Data Science"' },
+            { name: "level: 6" },
+            { name: 'department: "department._id"' }
+          ]
+        },
+        {
+          id: 3,
+          name: "offer",
+          children: [
+            { name: 'course: "course._id"' },
+            { name: "year: 2016" },
+            { name: "classSize: 40" },
+            { name: "availablePlaces: 39" },
+            { name: "enrolledCount: 1" }
+          ]
+        },
+        {
+          id: 4,
+          name: "students",
+          children: [
+            { name: '_id: "15101010"' },
+            { name: 'stuName: "Chan Tai Man"' },
+            { name: 'dOB: "10/08/2009"' },
+            {
+              id: 41,
+              name: "enrolled",
+              children: [
+                { name: 'courseID: "CS101"' },
+                { name: 'enrolDate: "15/05/2016"' }
+              ]
+            }
+          ]
+        }
+      ],
+      model2: [
+        {
+          id: 1,
+          name: "departments",
+          children: [
+            { name: '_id: "ObjectId"' },
+            { name: 'deptId: "CS"' },
+            { name: 'deptName: "Computer Science"' },
+            { name: 'location: "Green Zone"' },
+            {
+              id: 11,
+              name: "courses",
+              children: [{ name: "course._id" }, { name: "course._id" }]
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: "courses",
+          children: [
+            { name: '_id: "ObjectId"' },
+            { name: 'courseId: "CS101"' },
+            { name: 'title: "Introduction to Data Science"' },
+            { name: "level: 6" }
+          ]
+        },
+        {
+          id: 3,
+          name: "offer",
+          children: [
+            { name: 'department: "department._id"' },
+            { name: 'course: "course._id"' },
+            { name: "year: 2016" },
+            { name: "classSize: 40" },
+            { name: "availablePlaces: 39" },
+            { name: "enrolledCount: 1" },
+            {
+              id: 31,
+              name: "enrolled",
+              children: [
+                { name: 'student: "15101010"' },
+                { name: 'enrolDate: "15/05/2016"' }
+              ]
+            }
+          ]
+        },
+        {
+          id: 4,
+          name: "students",
+          children: [
+            { name: '_id: "15101010"' },
+            { name: 'stuName: "Chan Tai Man"' },
+            { name: 'dOB: "10/08/2009"' }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
