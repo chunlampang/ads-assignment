ebc1f0e4743797912ff9b30be4553dbaae50d336
Pang Chun Lam <chunlampang@gmail.com>
2019-08-08 00:41:47 +0800

    add v-card

--- vue/src/components/maint/EditForm.vue
+++ vue/src/components/maint/EditForm.vue
@@ -1,14 +1,16 @@
 <template>
   <v-form ref="form" v-model="valid" @submit.prevent="submit">
-    <v-card-text>
-      <BaseAlert v-if="alert.self" :value="alert.show" :type="alert.type" :msg="alert.msg" />
-      <EditFields v-model="value" :id="id" :fields="fields" />
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer />
-      <v-btn @click="reset" color="warning" text class="text-none">Reset</v-btn>
-      <v-btn type="submit" :disabled="!valid" color="primary" text class="text-none">Submit</v-btn>
-    </v-card-actions>
+    <v-card flat>
+      <v-card-text>
+        <BaseAlert v-if="alert.self" :value="alert.show" :type="alert.type" :msg="alert.msg" />
+        <EditFields v-model="value" :id="id" :fields="fields" />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn @click="reset" color="warning" text class="text-none">Reset</v-btn>
+        <v-btn type="submit" :disabled="!valid" color="primary" text class="text-none">Submit</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-form>
 </template>
 <script>
