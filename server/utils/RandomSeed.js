4d8624728132d4d575718fc1763af228f2926c01
Pang Chun Lam <chunlampang@gmail.com>
2019-08-08 00:26:24 +0800

    add cursor

--- server/utils/RandomSeed.js
+++ server/utils/RandomSeed.js
@@ -2,10 +2,11 @@ module.exports = class RandomSeed {
 
     constructor(seed = Math.random()) {
         this.seed = seed;
+        this.cursor = seed;
     }
 
     random() {
-        let x = Math.sin(this.seed++) * 10000;
+        let x = Math.sin(this.cursor++) * 10000;
         return x - Math.floor(x);
     }
 };
