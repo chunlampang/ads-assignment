f101daabf486240b1f7db17aa7bc81200b34cfed
Pang Chun Lam <chunlampang@gmail.com>
2019-08-08 18:48:25 +0800

    handle startup error

--- server/index.js
+++ server/index.js
@@ -1,29 +1,21 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
-const mongoPool = require('./utils/mongoPool');
 const config = require('./config');
 
-(async function init() {
-  try {
-    await mongoPool.connect();
+const app = express();
+//api
+app.use('/api', require('./api'));
 
-    const app = express();
-    //api
-    app.use('/api', require('./api'));
+//static route
+const staticFileMiddleware = express.static('public');
+app.use(staticFileMiddleware);
+app.use(history());
+app.use(staticFileMiddleware);
 
-    //static route
-    const staticFileMiddleware = express.static('public');
-    app.use(staticFileMiddleware);
-    app.use(history());
-    app.use(staticFileMiddleware);
-
-    //start server
-    let port = config.server.port;
-    app.listen(port, '0.0.0.0', () => {
-      console.log('Server start listening on http://localhost' + (port == 80 ? '' : ':' + port));
-    });
-  } catch (err) {
-    console.error(err.message);
-    process.exit(0);
-  }
-})();
+//start server
+let port = config.server.port;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Server start listening on http://localhost' + (port == 80 ? '' : ':' + port));
+}).on('error', function (err) {
+  console.error(err.message);
+});
