a8af1b52a684d3f09b99f032e3917a6509489221
Pang Chun Lam <30290516+chunlampang@users.noreply.github.com>
2019-08-26 16:46:12 +0800

    Update departments.data.js

--- server/data/departments.data.js
+++ server/data/departments.data.js
@@ -2,133 +2,129 @@ module.exports = [
     {
         deptId: 'CA', deptName: 'Architecture and Civil Engineering', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS104', title: 'Course 04', level: 5 } },
-            { course: { courseId: 'CS105', title: 'Course 05', level: 5 } },
-            { course: { courseId: 'CS106', title: 'Course 06', level: 5 } },
-            { course: { courseId: 'CS107', title: 'Course 07', level: 5 } },
-            { course: { courseId: 'CS108', title: 'Course 08', level: 5 } },
+            { course: { courseId: 'AC104', title: 'Professionals and Society', level: 4 } },
+            { course: { courseId: 'AC105', title: 'Engineering Communication', level: 5 } },
+            { course: { courseId: 'AC106', title: 'General Physics I', level: 3 } },
+            { course: { courseId: 'AC107', title: 'Chemistry', level: 5 } },
+            { course: { courseId: 'AC108', title: 'Discovery in Biology', level: 2 } },
         ]
     },
     {
         deptId: 'CS', deptName: 'Computer Science', location: 'Green Zone',
         courses: [
             { course: { courseId: 'CS101', title: 'Introduction to Data Science', level: 6 } },
-            { course: { courseId: 'CS102', title: 'Introduction to Hello World', level: 4 } },
+            { course: { courseId: 'CS102', title: 'Introduction to Computer Science', level: 4 } },
             { course: { courseId: 'CS103', title: 'Introduction to MongoDB', level: 5 } },
         ]
     },
     {
         deptId: 'IS', deptName: 'Information Systems', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS109', title: 'Course 09', level: 5 } },
-            { course: { courseId: 'CS110', title: 'Course 10', level: 5 } },
-            { course: { courseId: 'CS111', title: 'Course 11', level: 5 } },
-            { course: { courseId: 'CS112', title: 'Course 12', level: 5 } },
-            { course: { courseId: 'CS113', title: 'Course 13', level: 5 } },
-            { course: { courseId: 'CS114', title: 'Course 14', level: 5 } },
-            { course: { courseId: 'CS115', title: 'Course 15', level: 5 } },
+            { course: { courseId: 'CS109', title: 'Mobile Applications for Business', level: 6 } },
+            { course: { courseId: 'CS110', title: 'Big Data Management', level: 5 } },
+            { course: { courseId: 'CS111', title: 'Business Intelligence and Analytics', level: 5 } },
+            { course: { courseId: 'CS112', title: 'Global Business Technology Consulting', level: 7 } },
+            { course: { courseId: 'CS113', title: 'Global Project Management', level: 3 } },
+            { course: { courseId: 'CS114', title: 'Global Technology Entrepreneurship and Innovation', level: 5 } },
+            { course: { courseId: 'CS115', title: 'Social Media and Social Networks', level: 5 } },
         ]
     },
     {
         deptId: 'AC', deptName: 'Accountancy', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS116', title: 'Course 16', level: 5 } },
-            { course: { courseId: 'CS117', title: 'Course 17', level: 5 } },
-            { course: { courseId: 'CS118', title: 'Course 18', level: 5 } },
-            { course: { courseId: 'CS119', title: 'Course 19', level: 5 } },
-            { course: { courseId: 'CS120', title: 'Course 20', level: 5 } },
-            { course: { courseId: 'CS121', title: 'Course 21', level: 5 } },
-            { course: { courseId: 'CS122', title: 'Course 22', level: 5 } },
-        ]
-    },
-    {
-        deptId: 'AI', deptName: 'Asian and International Studies', location: 'Green Zone',
-        courses: [
-            { course: { courseId: 'CS123', title: 'Course 23', level: 5 } },
-            { course: { courseId: 'CS124', title: 'Course 24', level: 5 } },
-            { course: { courseId: 'CS125', title: 'Course 25', level: 5 } },
-            { course: { courseId: 'CS130', title: 'Course 30', level: 5 } },
-            { course: { courseId: 'CS131', title: 'Course 31', level: 5 } },
-            { course: { courseId: 'CS132', title: 'Course 32', level: 5 } },
+            { course: { courseId: 'CS116', title: 'University English', level: 5 } },
+            { course: { courseId: 'CS117', title: 'Business Case Analysis and Communication', level: 5 } },
+            { course: { courseId: 'CS118', title: 'Financial Statement Analysis', level: 5 } },
+            { course: { courseId: 'CS119', title: 'Introduction to Financial Markets', level: 5 } },
+            { course: { courseId: 'CS120', title: 'Advanced Seminar on Business Case Analysis', level: 5 } },
+            { course: { courseId: 'CS121', title: 'Business and Law', level: 5 } },
+            { course: { courseId: 'CS122', title: 'Business Programming with Spreadsheet', level: 5 } },
         ]
     },
     {
         deptId: 'BE', deptName: 'Biomedical Engineering', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS133', title: 'Course 33', level: 5 } },
-            { course: { courseId: 'CS134', title: 'Course 34', level: 5 } },
-            { course: { courseId: 'CS135', title: 'Course 35', level: 5 } },
-            { course: { courseId: 'CS136', title: 'Course 36', level: 5 } },
-            { course: { courseId: 'CS137', title: 'Course 37', level: 5 } },
-            { course: { courseId: 'CS138', title: 'Course 38', level: 5 } },
-            { course: { courseId: 'CS139', title: 'Course 39', level: 5 } },
-            { course: { courseId: 'CS140', title: 'Course 40', level: 5 } },
-            { course: { courseId: 'CS141', title: 'Course 41', level: 5 } },
+            { course: { courseId: 'CS123', title: 'Calculus and Basic Linear Algebra', level: 5 } },
+            { course: { courseId: 'CS124', title: 'Introduction to Computer Studies ', level: 5 } },
+            { course: { courseId: 'CS125', title: 'General Physics I', level: 5 } },
+            { course: { courseId: 'CS130', title: 'Chemistry', level: 5 } },
+            { course: { courseId: 'CS131', title: 'University English & Discipline Specific English', level: 5 } },
+            { course: { courseId: 'CS132', title: 'English for Engineering', level: 5 } },
         ]
+    },
+    {
+        deptId: 'AP', deptName: 'Applied Physics', location: 'Green Zone',
+        courses: [
+            { course: { courseId: 'CS133', title: 'Enhanced Calculus and Linear Algebra I', level: 5 } },
+            { course: { courseId: 'CS134', title: 'Introduction to Computer Programming', level: 5 } },
+            { course: { courseId: 'CS135', title: 'Chinese Civilization', level: 5 } },
+            { course: { courseId: 'CS136', title: 'English for Science', level: 5 } },
+            { course: { courseId: 'CS137', title: 'University English', level: 5 } },
+            { course: { courseId: 'CS138', title: 'Discovery in Biology', level: 5 } },
+          ]
     },
     {
         deptId: 'BS', deptName: 'Biomedical Sciences', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS142', title: 'Course 42', level: 5 } },
-            { course: { courseId: 'CS143', title: 'Course 43', level: 5 } },
-            { course: { courseId: 'CS144', title: 'Course 44', level: 5 } },
-            { course: { courseId: 'CS145', title: 'Course 45', level: 5 } },
-            { course: { courseId: 'CS146', title: 'Course 46', level: 5 } },
+            { course: { courseId: 'CS142', title: 'Human Structure and Function', level: 5 } },
+            { course: { courseId: 'CS143', title: 'Biochemistry of Human Body', level: 5 } },
+            { course: { courseId: 'CS144', title: 'Biostatistics and Epidemiology', level: 5 } },
+            { course: { courseId: 'CS145', title: 'Introduction to Bioethics', level: 5 } },
+            { course: { courseId: 'CS146', title: 'Immunology', level: 5 } },
         ]
     },
     {
         deptId: 'CM', deptName: 'Chemistry', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS155', title: 'Course 55', level: 5 } },
-            { course: { courseId: 'CS156', title: 'Course 56', level: 5 } },
-            { course: { courseId: 'CS157', title: 'Course 57', level: 5 } },
-            { course: { courseId: 'CS158', title: 'Course 58', level: 5 } },
+            { course: { courseId: 'CS155', title: 'Organic Chemistry', level: 5 } },
+            { course: { courseId: 'CS156', title: 'Application of Computer in Chemistry', level: 5 } },
+            { course: { courseId: 'CS157', title: 'Polymer Chemistry', level: 5 } },
+            { course: { courseId: 'CS158', title: 'Green Chemistry', level: 5 } },
         ]
     },
     {
         deptId: 'CH', deptName: 'Chinese and History', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS168', title: 'Course 68', level: 5 } },
-            { course: { courseId: 'CS169', title: 'Course 69', level: 5 } },
-            { course: { courseId: 'CS170', title: 'Course 70', level: 5 } },
-            { course: { courseId: 'CS171', title: 'Course 71', level: 5 } },
-            { course: { courseId: 'CS172', title: 'Course 72', level: 5 } },
-            { course: { courseId: 'CS173', title: 'Course 73', level: 5 } },
+            { course: { courseId: 'CS168', title: 'Classical Chinese fiction', level: 5 } },
+            { course: { courseId: 'CS169', title: 'Prose up to the nineteenth century', level: 5 } },
+            { course: { courseId: 'CS170', title: 'Study of the Confucian canons and modern society', level: 5 } },
+            { course: { courseId: 'CS171', title: 'Creative writing', level: 5 } },
+            { course: { courseId: 'CS172', title: 'Introduction to Chinese women¡¦s literature', level: 5 } },
+            { course: { courseId: 'CS173', title: 'Classical Chinese literary criticism', level: 5 } },
         ]
     },
     {
         deptId: 'EF', deptName: 'Economics and Finance', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS174', title: 'Course 74', level: 5 } },
-            { course: { courseId: 'CS175', title: 'Course 75', level: 5 } },
-            { course: { courseId: 'CS176', title: 'Course 76', level: 5 } },
-            { course: { courseId: 'CS177', title: 'Course 77', level: 5 } },
-            { course: { courseId: 'CS178', title: 'Course 78', level: 5 } },
+            { course: { courseId: 'CS174', title: 'ECONOMICS AND SOCIETY', level: 5 } },
+            { course: { courseId: 'CS175', title: 'BASIC BANKING & FINANCE', level: 5 } },
+            { course: { courseId: 'CS176', title: 'BASIC MACROECONOMICS', level: 5 } },
+            { course: { courseId: 'CS177', title: 'FINANCIAL DATA ANALYSIS', level: 5 } },
+            { course: { courseId: 'CS178', title: 'MONEY AND BANKING', level: 5 } },
         ]
     },
     {
         deptId: 'EE', deptName: 'Electrical Engineering', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS179', title: 'Course 79', level: 5 } },
-            { course: { courseId: 'CS180', title: 'Course 80', level: 5 } },
-            { course: { courseId: 'CS181', title: 'Course 81', level: 5 } },
+            { course: { courseId: 'CS179', title: 'University English', level: 5 } },
+            { course: { courseId: 'CS180', title: 'General Physics', level: 5 } },
+            { course: { courseId: 'CS181', title: 'Foundations of Digital Techniques', level: 5 } },
         ]
     },
     {
         deptId: 'EN', deptName: 'English', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS182', title: 'Course 82', level: 5 } },
-            { course: { courseId: 'CS183', title: 'Course 83', level: 5 } },
-            { course: { courseId: 'CS184', title: 'Course 84', level: 5 } },
+            { course: { courseId: 'CS182', title: 'University English', level: 5 } },
+            { course: { courseId: 'CS183', title: 'English for Academic Purposes 1 & 2', level: 5 } },
         ]
     },
     {
         deptId: 'IP', deptName: 'Infectious Diseases and Public Health', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS185', title: 'Course 85', level: 5 } },
-            { course: { courseId: 'CS186', title: 'Course 86', level: 5 } },
-            { course: { courseId: 'CS187', title: 'Course 87', level: 5 } },
-            { course: { courseId: 'CS188', title: 'Course 88', level: 5 } },
+            { course: { courseId: 'CS185', title: 'Advanced epidemiological methods I', level: 5 } },
+            { course: { courseId: 'CS186', title: 'Advanced clinical epidemiology and decision analysis', level: 5 } },
+            { course: { courseId: 'CS187', title: 'Accounting and financial management in health care', level: 5 } },
+            { course: { courseId: 'CS188', title: 'Biological basis of disease', level: 5 } },
         ]
     },
     {
@@ -142,86 +138,86 @@ module.exports = [
     {
         deptId: 'MM', deptName: 'Management', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS192', title: 'Course 92', level: 5 } },
-            { course: { courseId: 'CS193', title: 'Course 93', level: 5 } },
-            { course: { courseId: 'CS194', title: 'Course 94', level: 5 } },
+            { course: { courseId: 'CS192', title: 'Business Environment', level: 5 } },
+            { course: { courseId: 'CS193', title: 'Financial Accounting', level: 5 } },
+            { course: { courseId: 'CS194', title: 'Project Work', level: 5 } },
         ]
     },
     {
         deptId: 'MS', deptName: 'Management Sciences', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS195', title: 'Course 95', level: 5 } },
-            { course: { courseId: 'CS196', title: 'Course 96', level: 5 } },
-            { course: { courseId: 'CS197', title: 'Course 97', level: 5 } },
+            { course: { courseId: 'CS195', title: 'Transportation, Assignment, and Transshipment Problems', level: 5 } },
+            { course: { courseId: 'CS196', title: 'Integer Linear Programming', level: 5 } },
+            { course: { courseId: 'CS197', title: 'Network Models', level: 5 } },
         ]
     },
     {
         deptId: 'MK', deptName: 'Marketing', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS198', title: 'Course 98', level: 5 } },
-            { course: { courseId: 'CS199', title: 'Course 99', level: 5 } },
-            { course: { courseId: 'CS200', title: 'Course 100', level: 5 } },
+            { course: { courseId: 'CS198', title: 'Organization and Management', level: 5 } },
+            { course: { courseId: 'CS199', title: 'Marketing Management', level: 5 } },
+            { course: { courseId: 'CS200', title: 'Consumer Behaviour', level: 5 } },
         ]
     },
     {
-        deptId: 'MA', deptName: 'Materials Science and Engineering', location: 'Green Zone',
+        deptId: 'SS', deptName: 'Statistics', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS147', title: 'Course 47', level: 5 } },
-            { course: { courseId: 'CS148', title: 'Course 48', level: 5 } },
-            { course: { courseId: 'CS149', title: 'Course 49', level: 5 } },
-            { course: { courseId: 'CS150', title: 'Course 50', level: 5 } },
+            { course: { courseId: 'CS147', title: 'Biostatistics', level: 5 } },
+            { course: { courseId: 'CS148', title: 'Evolutionary Diversity', level: 5 } },
+            { course: { courseId: 'CS149', title: 'From Molecules to Cells', level: 5 } },
+            { course: { courseId: 'CS150', title: 'Principles of Biochemistry', level: 5 } },
         ]
     },
     {
-        deptId: 'MT', deptName: 'Mathematics', location: 'Green Zone',
+        deptId: 'MT', deptName: 'Psychology and Wellness', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS151', title: 'Course 51', level: 5 } },
-            { course: { courseId: 'CS152', title: 'Course 52', level: 5 } },
-            { course: { courseId: 'CS153', title: 'Course 53', level: 5 } },
-            { course: { courseId: 'CS154', title: 'Course 54', level: 5 } },
+            { course: { courseId: 'CS151', title: 'Government and Politics of Hong Kong', level: 5 } },
+            { course: { courseId: 'CS152', title: 'Introduction to Chinese Politics', level: 5 } },
+            { course: { courseId: 'CS153', title: 'Introduction to Comparative Politics', level: 5 } },
+            { course: { courseId: 'CS154', title: 'Class and Inequality', level: 5 } },
         ]
     },
     {
-        deptId: 'ME', deptName: 'Mechanical Engineering', location: 'Green Zone',
+        deptId: 'JS', deptName: 'Japanese Studies', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS159', title: 'Course 59', level: 5 } },
-            { course: { courseId: 'CS160', title: 'Course 60', level: 5 } },
+            { course: { courseId: 'CS159', title: 'Studies of Classical Texts', level: 5 } },
+            { course: { courseId: 'CS160', title: 'Literary Linguistics', level: 5 } },
         ]
     },
     {
         deptId: 'MC', deptName: 'Media and Communication', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS163', title: 'Course 63', level: 5 } },
+            { course: { courseId: 'CS163', title: 'Media and Communication', level: 5 } },
         ]
     },
     {
-        deptId: 'PH', deptName: 'Physics', location: 'Green Zone',
+        deptId: 'BS', deptName: 'Bilingual Studies', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS164', title: 'Course 64', level: 5 } },
-            { course: { courseId: 'CS165', title: 'Course 65', level: 5 } },
+            { course: { courseId: 'CS164', title: 'English Phonetics and Phonology', level: 5 } },
+            { course: { courseId: 'CS165', title: 'Advanced Putonghua', level: 5 } },
         ]
     },
     {
-        deptId: 'PP', deptName: 'Public Policy', location: 'Green Zone',
+        deptId: 'ACC', deptName: 'Arts in Media, Cultural and Creative Studies', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS166', title: 'Course 66', level: 5 } },
-            { course: { courseId: 'CS167', title: 'Course 67', level: 5 } },
+            { course: { courseId: 'CS166', title: 'Introduction to New Media', level: 5 } },
+            { course: { courseId: 'CS167', title: 'Researching Culture and Media', level: 5 } },
         ]
     },
     {
         deptId: 'SB', deptName: 'Social and Behavioural Sciences', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS161', title: 'Course 61', level: 5 } },
-            { course: { courseId: 'CS162', title: 'Course 62', level: 5 } },
+            { course: { courseId: 'CS161', title: 'The Basics of Sociology', level: 5 } },
+            { course: { courseId: 'CS162', title: 'Introduction to Psychology', level: 5 } },
         ]
     },
     {
         deptId: 'SE', deptName: 'Systems Engineering and Engineering Management', location: 'Green Zone',
         courses: [
-            { course: { courseId: 'CS126', title: 'Course 26', level: 5 } },
-            { course: { courseId: 'CS127', title: 'Course 27', level: 5 } },
-            { course: { courseId: 'CS128', title: 'Course 28', level: 5 } },
-            { course: { courseId: 'CS129', title: 'Course 29', level: 5 } },
+            { course: { courseId: 'CS126', title: 'Operations Research', level: 5 } },
+            { course: { courseId: 'CS127', title: 'Engineering Economic Analysis', level: 5 } },
+            { course: { courseId: 'CS128', title: 'Engineering Graphics', level: 5 } },
+            { course: { courseId: 'CS129', title: 'Engineering Workshop Practice', level: 5 } },
         ]
     },
 ]
